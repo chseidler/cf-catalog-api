97bacc541da87a22d9074517d24f6b3e8c06c9a2
Christian Seidler <ch.cseidler@gmail.com>
2023-10-09 22:19:34 -0300

    CreateCategory Test

--- UnitTests/Application/CreateCategory/CreateCategoryTest.cs
+++ UnitTests/Application/CreateCategory/CreateCategoryTest.cs
@@ -1,4 +1,7 @@
+using Domain.Entity;
+using FluentAssertions;
 using Moq;
+using UseCases = Application.UseCases.CreateCategory;
 
 namespace UnitTests.Application.CreateCategory;
 
@@ -6,10 +9,24 @@ public class CreateCategoryTest
 {
     [Fact(DisplayName = nameof(CreateCategory))]
     [Trait("Application", "CreateCategory - Use Cases")]
-    public void CreateCategory()
+    public async void CreateCategory()
     {
         var repositoryMock = new Mock<ICategoryRepository>();
         var unitOfWorkMock = new Mock<IUnitOfWork>();
-        var useCase = new CreateCategory(repositoryMock.Object, unitOfWorkMock.Object);
+        var useCase = new UseCases.CreateCategory(repositoryMock.Object, unitOfWorkMock.Object);
+
+        var input = CreateCategoryInput("Category Name", "Category Description", true);
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        repositoryMock.Verify(repository => repository.Create(It.IsAny<Category>(), It.IsAny<CancellationToken>()), Times.Once);
+        unitOfWorkMock.Verify(uow => uow.Commit(It.IsAny<CancellationToken>()), Times.Once);
+
+        output.ShouldNotBeNull();
+        output.Name.Should().Be("Category Name");
+        output.Description.Should().Be("Category Description");
+        output.IsActive.Should().Be(true);
+        (output.Id != null && output.Id != Guid.Empty).Should.BeTrue();
+        (output.CreatedAt != null && output.CreatedAt != default).Should.BeTrue();
     }
 }
