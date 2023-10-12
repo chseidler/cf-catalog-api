e6ad2a149e18bb4a62ca93a03e16922d55f06ba3
Christian Seidler <ch.cseidler@gmail.com>
2023-10-11 22:25:55 -0300

    UnitOfWork interface

---
+++ Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+namespace Application.Interfaces;
+
+public interface IUnitOfWork
+{
+    public Task Commit(CancellationToken cancellationToken);
+}
--- UnitTests/Application/CreateCategory/CreateCategoryTest.cs
+++ UnitTests/Application/CreateCategory/CreateCategoryTest.cs
@@ -1,3 +1,4 @@
+using Application.Interfaces;
 using Domain.Entity;
 using Domain.Repository;
 using FluentAssertions;
